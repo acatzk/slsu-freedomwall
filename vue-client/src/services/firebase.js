6dc1bb079982f05521b03907bebb27894fbbfea7
Joshua Galit <joshuaimalay@gmail.com>
2020-11-29 18:42:20 +0800

    Initialize firebase Applciation and configuration

---
+++ vue-client/src/services/firebase.js
@@ -0,0 +1,14 @@
+import firebase from '@firebase/app'
+import '@firebase/auth'
+import '@firebase/firestore'
+
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+})
+
+export default firebase
