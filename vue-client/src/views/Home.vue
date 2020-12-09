b661f98b7d7edf7efea3457e4b00a6989f260e77
Joshua Galit <joshuaimalay@gmail.com>
2020-12-09 21:08:28 +0800

    Import MainBar vue component and passed the slot default

--- vue-client/src/views/Home.vue
+++ vue-client/src/views/Home.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="home">
     <nav-bar />
-    <v-container>
-      <v-main>
-        <router-view></router-view>
-      </v-main>
-    </v-container>
+    <main-bar>
+      <v-container fluid>
+         <router-view></router-view>
+      </v-container>
+    </main-bar>
   </div>
 </template>
 
 <script>
-  import { mapActions, mapGetters } from 'vuex'
   export default {
-    name: 'home',
+    name: 'Home',
     components: {
-      NavBar: () => import('@/components/layouts/NavBar')
+      NavBar: () => import('@/components/layouts/NavBar'),
+      MainBar: () => import('@/components/layouts/MainBar')
     }
   }
 </script>
 
-<style scoped lang="scss">
+<style scoped>
   .home {
-    min-height: 1000vh;
     background:#f0f2f5;
+    height: 1000vh;
   }
 </style>
