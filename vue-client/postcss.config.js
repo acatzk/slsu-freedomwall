ce6f9f2c6b69450698ac62ce88e3a40d3b08ec3e
Joshua Galit <joshuaimalay@gmail.com>
2020-12-11 20:20:55 +0800

    Hide the fullhuman postcss config

--- vue-client/postcss.config.js
+++ vue-client/postcss.config.js
@@ -5,12 +5,12 @@ module.exports = {
   plugins: [
     tailwindcss,
     autoprefixer,
-    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
-      content: [
-        './src/**/*.vue',
-        './public/index.html'
-      ],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
+    // process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+    //   content: [
+    //     './src/**/*.vue',
+    //     './public/index.html'
+    //   ],
+    //   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    // })
   ]
 }
