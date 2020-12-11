bc27e147ed29d8eb88da78e1b52f76c2c876d347
Joshua Galit <joshuaimalay@gmail.com>
2020-12-11 21:19:33 +0800

    Removed on production purge css

--- vue-client/postcss.config.js
+++ vue-client/postcss.config.js
@@ -4,13 +4,6 @@ const tailwindcss = require('tailwindcss')
 module.exports = {
   plugins: [
     tailwindcss,
-    autoprefixer,
-    process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
-      content: [
-        './src/**/*.vue',
-        './public/index.html'
-      ],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
+    autoprefixer
   ]
 }
