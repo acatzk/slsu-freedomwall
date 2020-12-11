6714d04913c67c16cd798eb8e5afc677ac3b46e8
Joshua Galit <joshuaimalay@gmail.com>
2020-12-11 16:13:09 +0800

    Optimized tailwind css utility using purgecss configuration

--- vue-client/postcss.config.js
+++ vue-client/postcss.config.js
@@ -4,6 +4,13 @@ const tailwindcss = require('tailwindcss')
 module.exports = {
   plugins: [
     tailwindcss,
-    autoprefixer
+    autoprefixer,
+    process.env.NODE_ENV === 'production' && require('@/fullhuman/postcss-purgecss')({
+      content: [
+        './src/**/*.vue',
+        './public/index.html'
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
   ]
 }
