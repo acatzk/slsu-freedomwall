85622bb661b6d3dfc5437454c99aca243aa2e88d
Joshua Galit <joshuaimalay@gmail.com>
2020-12-11 12:59:53 +0800

    Import NavBar and show only if user loggedIn

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -1,7 +1,23 @@
 <template>
-  <v-app>
-    <router-view></router-view>
+  <v-app class="h-screen w-full">
+    <nav-bar v-if="loggedIn"/>
+    <router-view class="bg-gray-100"></router-view>
   </v-app>
 </template>
+
+<script>
+  import { mapGetters } from 'vuex'
+  export default {
+    name: 'app',
+    components: {
+      NavBar: () => import('@/components/layouts/NavBar')
+    },
+    computed: {
+      ...mapGetters('user', {
+        loggedIn: 'loggedIn'
+      })
+    }
+  }
+</script>
 
 <style lang="scss" src="@/assets/app.scss"></style>
