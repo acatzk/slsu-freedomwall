c276029ae00a2f44f5d739d3df64fc964df2567f
Joshua Galit <joshuaimalay@gmail.com>
2020-12-22 09:59:50 +0800

    Closed menu svg icon when drawer = !drawer

--- vue-client/src/components/layouts/NavBar.vue
+++ vue-client/src/components/layouts/NavBar.vue
@@ -9,17 +9,19 @@
              text
              class="secondary--text focus:outline-none"
              @click.stop="drawer = !drawer">
-        <svg class="w-6 h-6" 
-             fill="none" 
-             stroke="currentColor" 
-             viewBox="0 0 24 24" 
-             xmlns="http://www.w3.org/2000/svg">
+        <svg  v-if="drawer === false"
+              class="w-6 h-6" 
+              fill="none" 
+              stroke="currentColor" 
+              viewBox="0 0 24 24" 
+              xmlns="http://www.w3.org/2000/svg">
           <path stroke-linecap="round" 
                 stroke-linejoin="round" 
                 stroke-width="2" 
                 d="M4 6h16M4 12h8m-8 6h16">
           </path>
         </svg>
+        <svg v-else class="w-6 h-6" stroke="currentColor" fill="none" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" data-attributes-set=",xmlns:xlink,xmlns,viewBox,preserveAspectRatio"><path xmlns="http://www.w3.org/2000/svg" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
       </v-btn>
       <v-btn small
              to="/en/search"
